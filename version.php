0ba0970d0f38bbab4635da4c09834f7249862155
pch312 <17672737+pch312@users.noreply.github.com>
2024-07-23 23:45:57 +0200

    Add files via upload

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
     $plugin->component = 'block_accessibilite';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494).
-    $plugin->version = 2024072003;  // YYYYMMDDHH (year, month, day, 24-hr time).
+    $plugin->version = 2024072301;  // YYYYMMDDHH (year, month, day, 24-hr time).
     $plugin->requires = 2020061500; // YYYYMMDDHH (This is the release version for Moodle 3.9).
     $plugin->maturity  = MATURITY_STABLE;
     $plugin->release = '1.0.0';
